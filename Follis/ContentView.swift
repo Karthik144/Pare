e3525bfc0cda51d4ac4fe9a1143096af8a4d9e5c
“Karthik144” <karthik.k.ramu@gmail.com>
2023-02-13 21:17:40 -0500

    Initial Commit

---
+++ Follis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Follis
+//
+//  Created by Karthik  Ramu on 2/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Follis/FollisApp.swift
@@ -0,0 +1,17 @@
+//
+//  FollisApp.swift
+//  Follis
+//
+//  Created by Karthik  Ramu on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct FollisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
