1c864c8bcc39e9e131a26801b678f51b61db2736
AkhilMah14 <akhil@maheeps.net>
2023-03-12 20:34:26 -0400

    merged changes

--- Follis/Consumer Views/CheckoutView.swift
+++ Follis/Consumer Views/CheckoutView.swift
@@ -68,7 +68,7 @@ struct CheckoutView: View {
                     let index = viewModel.cartItems.firstIndex(of: item)
                     //let itemTotal = viewModel.calcItemAddOnTotal(itemIndex: index ?? 0) + (Double(item.price) ?? 0.0)
  
-                    OrderItemCell(itemQuantity: item.quantity!, itemName: item.name, itemPrice: viewModel.total, rewardPoints: item.rewards, index: index!,finalTotal: total)
+                    OrderItemCell(itemQuantity: item.quantity!, itemName: item.name, itemPrice: viewModel.total, rewardPoints: item.rewards, index: index!, popup:false, finalTotal: total)
                         .padding(.leading, 25)
                         .padding(.trailing, 20)
 
@@ -82,7 +82,7 @@ struct CheckoutView: View {
 
                     Spacer()
 
-                    Text(String(viewModel.subtotal) + " USDC")
+                    Text(String(round(viewModel.subtotal * 100) / 100.0) + " USDC")
 
                 } //: HSTACK
                 .padding(.top, 10)
