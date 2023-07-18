f3e8c4095d492f6a1e24465d8bc4af806526649c
“Karthik144” <karthik.k.ramu@gmail.com>
2023-07-18 15:12:49 -0400

    Added view shops & mneu logic

--- Pare/Consumer Views/ShopsView.swift
+++ Pare/Consumer Views/ShopsView.swift
@@ -45,6 +45,27 @@ struct ShopsView: View {
     // MARK: - BODY
     var body: some View {
 
+        Group {
+
+            if authViewModel.userSession == nil {
+                subShopsView
+            } else {
+                mainShopsView
+            }
+
+        } //: GROUP 
+
+
+
+    } //: VIEW
+
+
+}
+
+extension ShopsView {
+
+    var mainShopsView: some View {
+
         NavigationView {
 
             VStack{
@@ -69,7 +90,7 @@ struct ShopsView: View {
 
 
                             VStack(alignment: .leading, spacing: 0){
-                                
+
                                 if let firstOrder = readyOrders.first {
                                     Text("Order Ready for Pickup")
                                         .foregroundColor(Color.white)
@@ -81,7 +102,7 @@ struct ShopsView: View {
                                         .font(.title2)
                                         .fontWeight(.bold)
                                 }
-                                
+
                                 HStack{
 
                                     if let firstOrder = readyOrders.first {
@@ -95,9 +116,9 @@ struct ShopsView: View {
                                             .foregroundColor(Color.white)
                                             .fontWeight(.light)
                                     }
-                                    
 
-                        
+
+
                                     Circle()
                                         .frame(width: 2, height: 2)
                                         .foregroundColor(Color.white)
@@ -131,7 +152,7 @@ struct ShopsView: View {
 
                 } //: IF ORDER READY
 
-                
+
                 /*
                 if authViewModel.currentUser?.wallet == false {
 
@@ -144,7 +165,7 @@ struct ShopsView: View {
 
                 } //: IF WALLET NOT FULLY SETUP
                  */
-                 
+
                 HStack{
                     Text("Near You")
                         .font(.title2)
@@ -180,7 +201,7 @@ struct ShopsView: View {
                         } //: FOR EACH
 
                     } //: LAZYVSTACK
-                    
+
 
                 } //: SCROLL VIEW
 
@@ -201,7 +222,7 @@ struct ShopsView: View {
                     }
                 }
 
-                
+
 
             } //: ON APPEAR
 
@@ -227,10 +248,203 @@ struct ShopsView: View {
 
         } //: ON APPEAR
 
+        
 
-    } //: VIEW
+    } //: MAIN SHOPS VIEW
+
+    var subShopsView: some View {
+
+//        NavigationView {
+
+            VStack{
+
+                SearchBar()
+                    .padding(.top, 10)
+
+                //Show ModalView Conditional Block
+                if orderReady == true{
 
 
+                    ZStack{
+
+                        RoundedRectangle(cornerRadius: 7, style: .continuous)
+                            .fill(Color.accentColor)
+                            .frame(width: 340, height: 75)
+
+
+                        HStack{
+
+                            Spacer()
+
+
+                            VStack(alignment: .leading, spacing: 0){
+
+                                if let firstOrder = readyOrders.first {
+                                    Text("Order Ready for Pickup")
+                                        .foregroundColor(Color.white)
+                                        .font(.title2)
+                                        .fontWeight(.bold)
+                                } else {
+                                    Text("Order Scheduled")
+                                        .foregroundColor(Color.white)
+                                        .font(.title2)
+                                        .fontWeight(.bold)
+                                }
+
+                                HStack{
+
+                                    if let firstOrder = readyOrders.first {
+                                        Text("Ready")
+                                            .font(.caption)
+                                            .foregroundColor(Color.white)
+                                            .fontWeight(.light)
+                                    } else {
+                                        Text("In Progress")
+                                            .font(.caption)
+                                            .foregroundColor(Color.white)
+                                            .fontWeight(.light)
+                                    }
+
+
+
+                                    Circle()
+                                        .frame(width: 2, height: 2)
+                                        .foregroundColor(Color.white)
+
+                                    Text(dateFormatter.string(from: pickUpTime!))
+                                        .font(.caption)
+                                        .foregroundColor(Color.white)
+                                        .fontWeight(.light)
+                                }
+                            }
+
+                            Spacer()
+
+
+                            Image(systemName: "clock.badge.checkmark")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 30, height: 30)
+
+
+                            Spacer()
+
+                        } //: HSTACK
+
+                    } //: ZSTACK
+                    .onTapGesture {
+                        showModal.toggle()
+                    }.sheet(isPresented: $showModal) {
+                        OrderStatusModalView(shop: "Yuanho Gao Chinese", address: "117 Maury Ave, Charlottesville, VA 22903", orderNumber: "#325", orderStatus: "In Progress", orderReady: $orderReady, showModal: $showModal)
+                    }
+
+                } //: IF ORDER READY
+
+
+                /*
+                if authViewModel.currentUser?.wallet == false {
+
+                    NavigationLink {
+                        FirstStoryView()
+                    } label: {
+                        AddFundsCell()
+
+                    }
+
+                } //: IF WALLET NOT FULLY SETUP
+                 */
+
+                HStack{
+                    Text("Near You")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding()
+                        .padding(.leading, 10)
+
+                    Spacer()
+                }
+
+                ScrollView {
+
+
+                    LazyVStack{
+
+                        ForEach(totalShops) { shop in
+
+                            Button {
+                                self.isActive = true
+
+                            } label: {
+                                ShopCell(shop: shop)
+                                    .padding(.leading, 0)
+                                    .padding()
+                            }
+                            .background(
+
+                                NavigationLink(destination: ShopItemView(rootIsActive: $isActive, shop: shop),
+                                                isActive: $isActive) {EmptyView()}
+                            )
+
+
+                        } //: FOR EACH
+
+                    } //: LAZYVSTACK
+
+
+                } //: SCROLL VIEW
+
+
+            } //: VSTACK
+            .navigationBarTitle("", displayMode: .inline)
+            .navigationViewStyle(.stack)
+//            .navigationBarTitle("")
+//            .navigationBarHidden(true)
+//            .navigationBarBackButtonHidden(false)
+            .onAppear(){
+                
+                viewModel.fetchShops { shops in
+                    self.totalShops = shops
+                }
+
+                viewModel.fetchAllOrders{ orders in
+
+                    for order in orders{
+                        if (order.status == "ready" || order.status == "pending"){
+                            self.pickUpTime = (orders[0].date_ordered).addingTimeInterval(20 * 60)
+                            orderReady = true
+                            if (order.status == "ready"){
+                                self.readyOrders.append(order)
+                            }
+                        }
+
+                    }
+                }
+
+                walletViewModel.getBalance(magic: magicSingleton.magic, userPublicAddress: authViewModel.currentUser?.public_address ?? "") { balance in
+
+                    if let balance = balance {
+                        tokenBalance = balance
+
+                    } else {
+                        print("Error retrieving balance. - ShopsView")
+                    }
+                }
+
+
+
+
+            } //: ON APPEAR
+
+
+//        } //: NAV VIEW
+//        .navigationViewStyle(StackNavigationViewStyle())
+
+
+
+
+    } //: SUB SHOPS VIEW
+
+    
 }
 
 
